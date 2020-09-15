2f42ecc5c9e5fd35ac60ed30f6731739a9de2dc7
Ray Olivier <ray.olivier@projekt202.com>
2020-09-15 16:22:50 -0500

    useState practice

--- 02-Basic-React-Hooks-useState-useRef-useEffect/1-my-coding-along/pages/index.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/1-my-coding-along/pages/index.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 
 const InputElement = () => {
-  const [inputText, setInputText] = useState('');
   // const inputText = results[0];
   // const setInputText = results[1];
+
+  const [inputText, setInputText] = useState('');
+  const [historyList, setHistoryList] = useState([]);
 
   return (
     <div>
       <input
         onChange={(e) => {
           setInputText(e.target.value);
+          setHistoryList([...historyList, e.target.value]);
         }}
         placeholder="Enter Some Text"
       />
       <br />
       {inputText}
+      <ul>
+        {historyList.map((x) => {
+          return <div>{x}</div>;
+        })}
+      </ul>
     </div>
   );
 };
